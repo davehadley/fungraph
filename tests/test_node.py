78214b638743ea4e622fd94f21caaf642ebf21f2
David Hadley <d.r.hadley@warwick.ac.uk>
2020-07-17 12:27:28 +0100

    add more tests.

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,7 +1,20 @@
 import operator
+import tempfile
+import timeit
 import unittest
+from time import sleep
+from typing import Any, Callable
 
 import graci
+
+
+def _slow_identity(x: Any, waitseconds: float = 1) -> Any:
+    sleep(waitseconds)
+    return x
+
+
+def _timeonce(f: Callable[[], Any]) -> float:
+    return timeit.timeit(f, number=1)
 
 
 class TestNode(unittest.TestCase):
@@ -16,7 +29,16 @@ class TestNode(unittest.TestCase):
 
     def test_node_arguments(self):
         result = graci.node(operator.add,
-                            graci.node(lambda : 2),
+                            graci.node(lambda: 2),
                             graci.node(lambda: 3),
                             ).compute()
         self.assertEqual(result, 5)
+
+    def test_cache(self):
+        cachedir = tempfile.mkdtemp()
+        node = graci.node(_slow_identity, 5, waitseconds=1)
+        f = lambda: node.compute(cachedir=cachedir)
+        t1 = _timeonce(f)
+        t2 = _timeonce(f)
+        self.assertGreater(t1, 0.5)
+        self.assertLess(t2, 0.5)
