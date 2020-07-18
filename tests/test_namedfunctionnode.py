8def2f286c9420c40c34dd8204beb238e7f77b80
David Hadley <d.r.hadley@warwick.ac.uk>
2020-07-18 15:29:40 +0100

    Add tests for named.

--- tests/test_namedfunctionnode.py
+++ tests/test_namedfunctionnode.py
@@ -9,8 +9,40 @@ class TestNamedFunctionNode(unittest.TestCase):
         return fungraph.named("name", lambda: None)
 
     def test_simple_named_graph(self):
+        node = fungraph.named("add", operator.add, 1, 2)
+        self.assertEqual(node.compute(), 3)
+        self.assertEqual(node.name, "add")
+        return
+
+    def test_retrieve_by_name(self):
         node = fungraph.named("add", operator.add,
                               fungraph.named("a", lambda: 1),
-                              fungraph.named("a", lambda: 2),
+                              fungraph.named("b", lambda: 2),
                               )
+        a = node["a"]
+        b = node["b"]
+        self.assertEqual(a.compute(), 1)
+        self.assertEqual(b.compute(), 2)
+        self.assertEqual(a.name, "a")
+        self.assertEqual(b.name, "b")
+        return
+
+    def test_retrieve_by_wrong_name_raises_keyerror(self):
+        node = fungraph.named("add", operator.add,
+                              fungraph.named("a", lambda: 1),
+                              fungraph.named("b", lambda: 2),
+                              )
+        with self.assertRaises(KeyError):
+            node["c"]
+        return
+
+    def test_mixed_named_unnamed_graph(self):
+        node = fungraph.fun(operator.add,
+                              fungraph.fun(lambda: 1),
+                              fungraph.named("b", lambda: 2),
+                              )
+        b = node["b"]
+        self.assertEqual(node.compute(), 3)
+        self.assertEqual(b.compute(), 2)
+        self.assertEqual(b.name, "b")
         return
