261e708eea18ec26331c5f39acf6da3924125568
David Hadley <d.r.hadley@warwick.ac.uk>
2020-07-22 14:13:04 +0100

    Automatically choose version to install for testrelease.

--- buildrelease.sh
+++ buildrelease.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+rm -rf fungraph.egg-info
 python3 setup.py sdist bdist_wheel
 
--- testrelease.sh
+++ testrelease.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+version=$(python -c "import fungraph; print(fungraph.__version__)")
 mkdir testrelease && cd testrelease
 python3 -m venv venvrelease
 source venvrelease/bin/activate
-pip install --no-cache-dir --upgrade fungraph
+pip install --no-cache-dir --upgrade "fungraph>=${version}"
 python -m unittest discover ../tests
