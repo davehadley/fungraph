afa1c2697dadfb4c2f529b40df2ed921dde3a4bd
David Hadley <d.r.hadley@warwick.ac.uk>
2020-07-23 13:29:31 +0100

    Improve testrelease script.

--- testrelease.sh
+++ testrelease.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-version=$(python -c "import fungraph; print(fungraph.__version__)")
-mkdir testrelease && cd testrelease
-python3 -m venv venvrelease
-source venvrelease/bin/activate
-pip install --no-cache-dir --upgrade "fungraph>=${version}"
-cp -r ../tests ./
-python -m unittest discover tests
+versionsource=$(cat fungraph/_version.py);
+version=$(python -c "${versionsource};print(__version__)");
+mkdir testrelease && cd testrelease && python3 -m venv venvrelease && source venvrelease/bin/activate && cp -r ../tests ./
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+pip install --no-cache-dir --upgrade "fungraph>=${version}" && python -m unittest discover tests
