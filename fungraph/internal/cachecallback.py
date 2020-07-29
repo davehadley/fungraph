80c8ad9aa9a22855dad6520193965c087562855e
David Hadley <d.r.hadley@warwick.ac.uk>
2020-07-29 13:07:46 +0100

    Hashing implementation passes tests but is slow.

--- fungraph/internal/cachecallback.py
+++ fungraph/internal/cachecallback.py
@@ -2,6 +2,8 @@ import hashlib
 from typing import Mapping, Tuple, Any
 
 import cloudpickle
+import dask
+import joblib
 from dask.callbacks import Callback
 
 
@@ -25,4 +27,40 @@ class CacheCallback(Callback):
 
 
 def dsktohash(dsk: Mapping[str, Tuple[Any, ...]]) -> Mapping[str, str]:
-    return {k: hashlib.md5(cloudpickle.dumps(dsk[k])).hexdigest() for k in dsk.keys()}
+    # from print import pprint
+    # pprint(dsk)
+    return {k: _hash(k, dsk) for k in dsk.keys()}
+
+
+def _hash(daskkey: str, dsk: Mapping[str, Tuple[Any, ...]]) -> str:
+    obj = dsk[daskkey]
+    if dask.core.istask(obj):
+        result = _hashtask(obj, dsk)
+    else:
+        result = _md5hashpickle(obj)
+    return result
+
+
+def _hashtask(obj: Any, dsk: Mapping[str, Tuple[Any, ...]]):
+    md5 = hashlib.md5("_hashtask".encode())
+    for o in obj:
+        if dask.core.istask(o):
+            h = _hashtask(o, dsk)
+        elif isinstance(o, str) and o in dsk:
+            h = _hash(o, dsk)
+        elif callable(o):
+            h = _get_callable_src(o)
+        elif isinstance(o, list):
+            h = _hashtask(o, dsk)
+        else:
+            h = _md5hashpickle(o)
+        md5.update(h.encode())
+    return md5.hexdigest()
+
+
+def _md5hashpickle(obj: Any) -> str:
+    return hashlib.md5(cloudpickle.dumps(obj)).hexdigest()
+
+
+def _get_callable_src(c):
+    return joblib.func_inspect.get_func_code(c)[0]
