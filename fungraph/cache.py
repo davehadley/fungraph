f9cb0093939eb976b15f333bf01901ec0c432b6b
David Hadley <d.r.hadley@warwick.ac.uk>
2020-08-03 23:42:05 +0100

    Refactor.

--- fungraph/cache.py
+++ fungraph/cache.py
@@ -2,10 +2,8 @@ from typing import Union, Mapping, Any, Optional, Iterable
 
 import dask
 from dask.callbacks import Callback
-from toolz import curry
 
-from fungraph.internal.cachecallback import CacheCallback
-from fungraph.internal.dsktohash import dsktohash
+from fungraph.internal.delayedoptimize import delayedoptimize
 from fungraph.internal.lockedcache import LockedCache
 
 DEFAULT_CACHE_PATH = ".fungraphcache"
@@ -33,40 +31,7 @@ def cachecontext(cache: Union[str, Mapping[str, Any], None] = DEFAULT_CACHE_PATH
         return Callback()
     if isinstance(cache, str):
         cache = LockedCache(cache)
-    return dask.config.set(delayed_optimize=_optimize(cache=cache))
+    return dask.config.set(delayed_optimize=delayedoptimize(cache=cache))
 
 
-class CachedResult():
-    def __init__(self, value):
-        self._value = value
 
-    def __call__(self, *args, **kwargs):
-        return self._value
-
-class CacheAfterExecution():
-    def __init__(self, f, cache, hash):
-        self._cache = cache
-        self._hash = hash
-        self._f = f
-
-    def __call__(self, *args, **kwargs):
-        print("calling:", self._f, args, kwargs)
-        value = self._f(*args, **kwargs)
-        self._cache[self._hash] = value
-        return value
-
-@curry
-def _optimize(dsk: Mapping[str, Any], keys: Optional[Union[str, Iterable[str]]] = None,
-              cache: Optional[Mapping[str, Any]] = None) -> Mapping[str, Any]:
-    hashes = dsktohash(dsk)
-    result = {}
-    for key in dsk:
-        try:
-            result[key] = cache[hashes[key]] #(CachedResult(cache[hashes[key]]), )
-        except KeyError:
-            c = dsk[key]
-            if dask.istask(c):
-                result[key] = (CacheAfterExecution(c[0], cache, hashes[key]),) + c[1:]
-            else:
-                result[key] = dsk[key]
-    return result
