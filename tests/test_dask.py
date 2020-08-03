112a2987de4832e4636e41430a409a83a12a08b8
David Hadley <d.r.hadley@warwick.ac.uk>
2020-08-03 21:43:16 +0100

    Add a unit test showing cache failure using dask distributed.

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -2,6 +2,7 @@ import operator
 import random
 import time
 import unittest
+from tempfile import TemporaryDirectory
 
 import dask.distributed
 from dask import delayed
@@ -47,6 +48,26 @@ class TestDask(unittest.TestCase):
             t1 = timeonce(lambda: slowfunc(1.0))
             tn = timeonce(jobs.cachedcompute)
             self.assertLess(tn, (t1 * N) / 2.0)
+
+    def test_paralell_uses_cache(self):
+        cluster = dask.distributed.LocalCluster(n_workers=8,
+                                                processes=True,
+                                                threads_per_worker=1,
+                                                memory_limit="auto")
+        with dask.distributed.Client(address=cluster):
+            def slowfunc(loc):
+                time.sleep(1)
+                return random.gauss(loc, 1.0)
+
+            N = 8
+            args = [fungraph.fun(slowfunc, random.uniform(0.0, 1.0)) for _ in range(N)]
+            jobs = fungraph.fun(lambda *args: sum(args), *args)
+            with TemporaryDirectory() as d:
+                with fungraph.cachecontext(d):
+                    t1 = timeonce(jobs.cachedcompute)
+                    t2 = timeonce(jobs.cachedcompute)
+            self.assertGreater(t1, 1.0)
+            self.assertLess(t2, 0.5)
 
     def test_repr(self):
         node = fungraph.fun(delayed(operator.add), 2, 3)
